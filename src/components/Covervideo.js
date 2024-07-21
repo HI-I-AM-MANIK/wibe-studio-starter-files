3d439fb4251afc2899cbd62872f08f9d3eefc1aa
John Doe <manikgupta2k04@gmail.com>
2024-07-22 03:35:42 +0530

    added video and components folder

--- src/components/Covervideo.js
+++ src/components/Covervideo.js
@@ -1,16 +1,66 @@
 import React from 'react'
 import styled from 'styled-components'
+import MainVideo from '../assets/Walking Girl.mp4'
 
 const VideoContainer = styled.section`
 width:100%;
-height:100vh;
+height:200vh;
 position:relative;
+video{
+    width: 100%;
+    height: 100vh;
+    object-fit:cover;
+}
 `
+
+const DarkOverLay = styled.div`
+position:absolute;
+top:0;
+left:0;
+bottom:0;
+right:0;
+z-index:1;
+
+background-color: ${props => `rgba(${props.theme.bodyRgba},0.6)`};
+`
+
+const Title = styled.div`
+position:absolute;
+top:0;
+left:0;
+bottom:0;
+right:0;
+z-index:1;
+
+display: flex;
+justify-content:center;
+align-items:center;
+color: ${props => props.theme.text};
+
+h1{
+    font-family:'Kaushan Script';
+    font-size:${props => props.theme.fontBig}
+}
+h2{
+    font-family:'Kaushan Script';
+    font-size:${props => props.theme.font};
+
+
+}
+`
+ 
 const Covervideo = () => {
   return (
-    <div>
-      
-    </div>
+    <VideoContainer>
+        <DarkOverLay/>
+        <Title>
+            <div data-scroll data-scroll-speed="4">
+                <h1>Wibe</h1>
+                <h2>Inspire.Create.Believe</h2>
+            </div>
+        </Title>
+        <video src = {MainVideo} type="video/mp4 " autoPlay muted loop />
+    </VideoContainer>
   )
 }
 
