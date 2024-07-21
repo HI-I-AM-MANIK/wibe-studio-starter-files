f5285a7d5b5a1d65af73ce8a970216ce86565a86
John Doe <manikgupta2k04@gmail.com>
2024-07-22 03:33:58 +0530

    added video and data-scroll

---
+++ src/components/Covervideo.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const VideoContainer = styled.section`
+width:100%;
+height:100vh;
+position:relative;
+`
+const Covervideo = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Covervideo
--- src/section/Home.js
+++ src/section/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import Covervideo from '../components/Covervideo'
 
 const Section = styled.section`
     position:relative;
@@ -9,9 +10,11 @@ const Section = styled.section`
 `
 const Home = () => {
   return (
-    <div>
-      Home
-    </div>
+    <Section>
+        <Covervideo/>
+        <h1>Navbar</h1>
+        <h1>Logo</h1>
+    </Section>
   )
 }
 
