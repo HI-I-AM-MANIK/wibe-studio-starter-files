cb792a0c9def7edc1b3935f30e0d65c3b356a866
John Doe <manikgupta2k04@gmail.com>
2024-07-22 02:54:23 +0530

    gsap hands on

---
+++ src/section/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Section = styled.section`
+    position:relative;
+    min-height:200vh;
+    overflow:hidden;
+    font-color:cyan;
+`
+const Home = () => {
+  return (
+    <div>
+      Home
+    </div>
+  )
+}
+
+export default Home
---
+++ src/styles/Themes.js
@@ -0,0 +1,25 @@
+//Add themes as per requirements
+
+export const dark ={
+    body: '#202020', //black shade
+    text: '#fff',
+    bodyRgba: '32,32,32',
+    textRgba: '255,255,255',
+
+    grey :'#bebebe',
+
+    fontxs:'0.75em', //1em = 10px 
+    fontsm:'0.875em',
+    fontxs:'1em',
+    fontmd:'1.25em',
+    fontlg:'2em',
+    fontxl:'3em',
+    fontxxl:'5em',
+    fontxxxl:'5em',
+    fontBig:'10em',
+
+    navHeight: '5rem'
+
+
+
+}
